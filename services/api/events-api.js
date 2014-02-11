c8d7f2b1e691c0e6f275f1a772230497039f0f26
Mike Barone <michael.a.barone@gmail.com>
2014-02-10 22:14:35 -0500

    added user validation via basic auth

--- services/api/events-api.js
+++ services/api/events-api.js
@@ -3,11 +3,15 @@ var appSettings = require('../lib/app-settings')
   , url = require('url')
   , eventsConnectionString = appSettings.connectionStrings.neo4j.events
   , graphDb = require('seraph')(createSeraphConnObj(eventsConnectionString))
+  , graphModel = require('seraph-model')
+  , Event = graphModel(graphDb, 'event')
+  , Profile = graphModel(graphDb, 'profile')
   , mysql = require('mysql')
   , users = require('../lib/users/user-model').table    
   , routes = require('./route-manager').createManager()
   ;
 
+// todo: refactor, duplicated (sorta)
 function createSeraphConnObj(endpoint) {
   var parsedUrl = url.parse(endpoint)
     , urlProto = parsedUrl.protocol
@@ -25,12 +29,16 @@ function createSeraphConnObj(endpoint) {
 }
 
 
-// todo: refactor, duplicated (almost)
-function completeRequest(res, code, desc, cont) {
+// todo: refactor, duplicated
+function completeRequest(db, res, code, desc, cont) {
   res.send(code, {
     description: desc,
     content: cont
   });
+
+  if (db) {
+    db.end();
+  }
 }
 
 // todo: refactored, duplicated
@@ -53,16 +61,47 @@ routes
     route: '/api/events',
     handler: function (req, res, next) {
       var eventInput = req.body
+        , username = req.username
+        , getUserQuery = getUserByUsernameQuery(username.toLowerCase())
+        , db
         ;
 
-      // temporary: echo back submitted input + interesting data
+      if (!username || username === 'anonymous') {
+        completeRequest(db, res, 400, 'error', 'missing username');
+        return;
+      }
 
-      eventInput.connectionData = {
-        raw: eventsConnectionString,
-        obj: createSeraphConnObj(eventsConnectionString)
-      };
+      db = mysql.createConnection(appSettings.connectionStrings.sql.users);
+      db.connect(function connectDb(err) {
+        
+        if (err) {
+          completeRequest(db, res, 500, 'error', err);
+          return;
+        }
 
-      completeRequest(res, 201, 'created', eventInput);
+        db.query(getUserQuery.text, getUserQuery.values, 
+          function execCount(err, rows) { 
+
+            if (err) {
+              completeRequest(db, res, 500, 'error', err);
+              return;
+            }
+
+            if (!rows || rows.length === 0) {
+              completeRequest(db, res, 400, 'error', 'user not found');
+            }
+
+            // temporary: echo back submitted input + interesting data
+
+            eventInput.connectionData = {
+              raw: eventsConnectionString,
+              obj: createSeraphConnObj(eventsConnectionString)
+            };
+
+            completeRequest(db, res, 201, 'created', eventInput);            
+          });
+      });
+
       return next();
     }
   });
